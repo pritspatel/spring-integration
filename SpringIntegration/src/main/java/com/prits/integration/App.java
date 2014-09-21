77df15f7e67297f808b25621773435989bcea5ac
Pritesh Patel <3ppatel@gmail.com>
2014-09-21 07:47:17 -0400

    -- Fixed application context loading issue

--- SpringIntegration/src/main/java/com/prits/integration/App.java
+++ SpringIntegration/src/main/java/com/prits/integration/App.java
@@ -5,7 +5,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
- * Hello world!
+ * Splitter Aggregator Sample
  * 
  */
 public class App {
